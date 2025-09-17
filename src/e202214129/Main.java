9bb7ccad971225b68a1f5ce8f9a3b8c2d85e9e58
Park, Juwon <won030702@gmail.com>
2025-09-17 16:51:51 +0900

    Six

--- src/e202214129/Main.java
+++ src/e202214129/Main.java
@@ -4,6 +4,6 @@ public class Main {
 	  public static void main(String[] args) {
 		  System.out.println("홍길동"); 
 		  System.out.println("안녕하세요");
-		  Hello.run();
+		  System.out.println("hello world");
 	  }
 }
