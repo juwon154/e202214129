d20f509ac8ac2777b24d9d3448353952799cb626
Park, Juwon <won030702@gmail.com>
2025-09-17 16:48:48 +0900

    Three

--- src/e202214129/Main.java
+++ src/e202214129/Main.java
@@ -2,7 +2,7 @@ package e202214129;
 
 public class Main {
 	  public static void main(String[] args) {
-		    System.out.println("박주원");
-		  }
-
+		  System.out.println("홍길동"); 
+		  System.out.println("안녕하세요");
+	  }
 }
